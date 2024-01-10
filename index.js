6f4ef68f770cc0f548fc1a7780c4e99b7a0543ac
Lavanya R <lra@softwareag.com>
2024-01-10 18:11:37 +0530

    fix: Fix integration error

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 // add validateConditions and prepare functions for semantic release plugin
 import verifyConfig from './lib/verify-config.js';
-import prepare from './lib/prepare.js';
+import preparePackage from './lib/prepare.js';
 import AggergateError from 'aggregate-error';
 
 let verified = false;
 
-export async function validateConditions(pluginConfig, context) {
-    const { logger } = context;
-    logger.log('pluginConfig', pluginConfig);
-    const errors = [];
-    if (!verified) {
-        errors = [...errors, ...await verifyConfig(pluginConfig, context)];
-        verified = true;
-    }
-    if(errors.length > 0){
-        throw new AggergateError(errors);
-    }
+export async function verifyConditions(pluginConfig, context) {
+  const { logger } = context;
+  logger.log('pluginConfig', pluginConfig);
+  const errors = await verifyConfig(pluginConfig, context);
+  verified = true;
+  if (errors.length > 0) {
+    throw new AggergateError(errors);
+  }
 }
 
 export async function prepare(pluginConfig, context) {
-    const errors = [];
-    if (!verified) {
-        errors = [...errors, ...await verifyConfig(pluginConfig, context)];
-        verified = true;
-    }
-    if(errors.length > 0){
-        throw new AggergateError(errors);
-    }
-    await prepare(pluginConfig, context);
+  const errors = verified ? [] : await verifyConfig(pluginConfig, context);
+  if (errors.length > 0) {
+    throw new AggergateError(errors);
+  }
+  try {
+    // TODO: Reload package in case it is updated in previous step
+    await preparePackage(pluginConfig, context);
+  } catch (error) {
+    throw new AggergateError([error.message]);
+  }
 }
